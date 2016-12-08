de8cf48b2186615509419a86613addfcad380a3b
vkupriya <vlad@evolvere-tech.co.uk>
2016-12-08 17:08:42 +0000

    Add files via upload

--- acli.py
+++ acli.py
@@ -8,7 +8,7 @@
 #                                                                              #
 ################################################################################
 #                                                                              #
-# Copyright 2016 Evolvere Technologies Ltd                                               #
+# Copyright 2016 Evolvere Technologies Ltd                                     #
 #                                                                              #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may   #
 #    not use this file except in compliance with the License. You may obtain   #
