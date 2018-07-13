7289e9805a9df4938cad45d444caacebe3135bee
vkupriya <vlad@evolvere-tech.co.uk>
2018-07-13 10:21:41 +0100

    Delete evoACISettings.py

--- evoACISettings.py
+++
@@ -1,7 +0,0 @@
-FABRICS = {'Sandbox': [
-    {'address': 'sandboxapicdc.cisco.com', 'username': 'admin', 'password': 'ciscopsdt'},
-     ],
-           'GLO': [
-    {'address': '10.50.137.92', 'username': 'admin', 'password': 'C1sco123'},
-     ],
-}
