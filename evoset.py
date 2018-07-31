f6987da4415917375b37797eff88a0d6f0dd0b30
vkupriya <vlad@evolvere-tech.co.uk>
2018-07-31 16:13:20 +0100

    Create evoset.py

---
+++ evoset.py
@@ -0,0 +1,92 @@
+FABRIC_MAP = {
+              'ukdc-dc0': 'Sandbox',
+              'ukdc-dc1': 'CiscoSVS',
+              'uspil-d01c1': 'PIC_F1',
+              'uspil-d01c2': 'PIC_F2',
+              'uspil-d01c3': 'PIC_F3',
+              'uspil-d01c4': 'PIC_F4',
+              'uscqg-d01c1': 'CRC_F1',
+              'uscqg-d01c3': 'CRC_F3',
+              'uscqg-d01c4': 'CRC_F4',
+              'gbglo-dc0': 'CiscoSVS',
+              'gbglo-dc1': 'GLO_F2',
+              'gbslo-d1': 'SLG_F1',
+              'gbslo-d2': 'SLG_F2',
+              'sgsgd-d2': 'SG1_F1',
+              'sgsge-d2': 'SG2_F1',
+             }
+
+
+FABRICS = {
+           'Sandbox': [
+    {'address': 'sandboxapicdc.cisco.com', 'username': 'admin', 'password': 'ciscopsdt'}
+     ],
+           'CiscoSVS': [
+        {'address': '3.1.9.141', 'username': 'admin', 'password': 'C1sco123'}
+    ],
+           'PIC_F1': [
+    {'address': 'uspil-d01c1-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c1-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c1-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'PIC_F2': [
+    {'address': 'uspil-d01c2-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c2-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c2-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'PIC_F3': [
+    {'address': 'uspil-d01c3-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c3-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c3-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'PIC_F4': [
+    {'address': 'uspil-d01c4-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c4-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'uspil-d01c4-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'CRC_F1': [
+    {'address': 'uscqg-d01c1-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'uscqg-d01c1-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'uscqg-d01c1-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'CRC_F3': [
+    {'address': 'uscqg-d01c3-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'uscqg-d01c3-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'uscqg-d01c3-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'CRC_F4': [
+    {'address': 'uscqg-d01c4-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'uscqg-d01c4-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'uscqg-d01c4-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'GLO_F1': [
+    {'address': 'gbglo-dc0-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'gbglo-dc0-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'gbglo-dc0-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'GLO_F2': [
+    {'address': 'gbglo-dc1-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'gbglo-dc1-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'gbglo-dc1-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'SLG_F1': [
+    {'address': 'gbslo-d1-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'gbslo-d1-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'gbslo-d1-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'SLG_F2': [
+    {'address': 'gbslo-d2-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'gbslo-d2-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'gbslo-d2-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'SG1_F1': [
+    {'address': 'sgsgd-d2-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'sgsgd-d2-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'sgsgd-d2-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+           'SG2_F1': [
+    {'address': 'sgsge-d2-ffcar-001.net.global', 'username': '', 'password': ''},
+    {'address': 'sgsge-d2-ffcar-002.net.global', 'username': '', 'password': ''},
+    {'address': 'sgsge-d2-ffcar-003.net.global', 'username': '', 'password': ''},
+     ],
+}
